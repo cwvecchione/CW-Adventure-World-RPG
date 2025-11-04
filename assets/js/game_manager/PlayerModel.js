3f00ca5efe4f1013e5c3875b987d05abd631fe34
Chris Vecchione <cwvecchione@gmail.com>
2025-11-04 17:11:25 -0500

    Updated stats for player and monsters, started building the inventory screen.

--- assets/js/game_manager/PlayerModel.js
+++ assets/js/game_manager/PlayerModel.js
@@ -1,10 +1,14 @@
 class PlayerModel {
   constructor(spawnLocations) {
-    this.health = 10;
-    this.maxHealth = 10;
+    tthis.attack = 25;
+    this.defense = 10;
+    this.health = 150;
+    this.maxHealth = 150;
     this.gold = 0;
     this.id = `player-${uuid.v4()}`;
     this.spawnLocations = spawnLocations;
+    this.playerItems = {};
+    this.maxNumberOfItems = 5;
 
     const location = this.spawnLocations[Math.floor(Math.random() * this.spawnLocations.length)];
     [this.x, this.y] = location;
@@ -16,12 +20,31 @@ class PlayerModel {
 
   updateHealth(health) {
     this.health += health;
-    if (this.health > 10) this.health = 10;
+    if (this.health > this.maxHealth) this.health = this.maxHealth;
+  }
+
+  playerAttacked(attack) {
+    const damage = this.defense - attack;
+    this.updateHealth(damage);
   }
 
   respawn() {
     this.health = this.maxHealth;
     const location = this.spawnLocations[Math.floor(Math.random() * this.spawnLocations.length)];
     [this.x, this.y] = location;
+  }
+
+  addItem(item) {
+    this.playerItems[item.id] = item;
+  }
+  removeItem(item) {
+    delete this.playerItems[item.id];
+  }
+
+  canPickupItem() {
+if (Object.keys(this.playerItems).length < this.maxNumberOfItems) {
+    return true;
+  }
+    return false;
   }
 }
